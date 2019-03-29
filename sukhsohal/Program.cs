98334471ec67ed32457c106f515a73c13f4cf922
visualstudio <visualstudio@124-PC18>
2019-03-28 22:00:33 -0400

    ASSIGNMENT3_C0731586

---
+++ sukhsohal/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sukhsohal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
